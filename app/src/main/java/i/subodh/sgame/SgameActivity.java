eb689da278773701081f1c0aa353a91c59691517
Your Name <you@example.com>
2017-08-17 17:07:50 +0530

    Initital project with activity defined

---
+++ app/src/main/java/i/subodh/sgame/SgameActivity.java
@@ -0,0 +1,13 @@
+package i.subodh.sgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SgameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sgame);
+    }
+}
