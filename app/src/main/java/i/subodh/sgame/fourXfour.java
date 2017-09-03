945fc13af0ac00d77feda8aa9433857c6120c1cf
Subodh Sharma <isubodh@gmail.com>
2017-09-03 15:54:22 +0530

    Added a 4x4 board n working

--- app/src/main/java/i/subodh/sgame/fourXfour.java
+++ app/src/main/java/i/subodh/sgame/fourXfour.java
@@ -17,22 +17,23 @@ import android.widget.Toast;
 
 import java.util.Random;
 
-public class SgameActivity extends AppCompatActivity implements View.OnClickListener{
+public class fourXfour extends AppCompatActivity implements View.OnClickListener{
 
-    private static final String TAG = "ANDRO-SGAME-3x3";
-    private static final int[] idBtn = {R.id.btn0, R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4,
-            R.id.btn5, R.id.btn6, R.id.btn7, R.id.btn8 };
+    private static final String TAG = "ANDRO-SGAME-4x4";
+    private static final int[] idBtn = {R.id.btx40, R.id.btx41, R.id.btx42, R.id.btx43, R.id.btx44,
+            R.id.btx45, R.id.btx46, R.id.btx47, R.id.btx48, R.id.btx49, R.id.btx410,R.id.btx411,
+            R.id.btx412,R.id.btx413,R.id.btx414,R.id.btx415  };
 
-    Button [] btnX = new Button[9];
-    private int [] cntrArr = new int[9];
+    Button[] btnX = new Button[16];
+    private int [] cntrArr = new int[16];
     private int ClickCount = 0;
-    Button btnReset;
-    Button btnHelp;
+    Button btx4Reset;
+    Button btx4Help;
     private TextView txtClickCount;
 
     /*
-    Set the Control Array Randomly
-     */
+Set the Control Array Randomly
+ */
     void setCntrArr(){
         Log.d(TAG,"Resetting the control array");
         //Set all to -1
@@ -54,8 +55,8 @@ public class SgameActivity extends AppCompatActivity implements View.OnClickList
         clickCounter("RESET");
     }
     /*
-    Verify the end game
-     */
+Verify the end game
+ */
     boolean isGameOver(){
         for (int i=0; i<cntrArr.length; i++){
             if (cntrArr[i] != i)
@@ -71,7 +72,7 @@ public class SgameActivity extends AppCompatActivity implements View.OnClickList
         Log.d(TAG, "Setting up Display of Buttons");
         for (int i=0; i < cntrArr.length; i++){
             btnX[i].setText(""+ (cntrArr[i] + 1));
-            if (cntrArr[i]   == cntrArr.length -1 )
+            if (cntrArr[i]   == cntrArr.length -1  )
                 btnX[i].setVisibility(View.INVISIBLE);
             else
                 btnX[i].setVisibility(View.VISIBLE);
@@ -84,39 +85,39 @@ public class SgameActivity extends AppCompatActivity implements View.OnClickList
         Log.d(TAG, "Handing User click");
 
         // Check the right side space
-        if (keyCode % 3 !=2 )
+        if (keyCode % 4 != 3 )
             if(cntrArr[keyCode+1] == cntrArr.length-1 ){
                 cntrArr[keyCode+1] = cntrArr[keyCode];
                 cntrArr[keyCode] = cntrArr.length-1;
                 clickCounter("INC");
             }
         //Check the Left side space
-        if (keyCode % 3 != 0)
+        if (keyCode % 4 != 0)
             if(cntrArr[keyCode-1] == cntrArr.length-1){
                 cntrArr[keyCode-1] = cntrArr[keyCode];
                 cntrArr[keyCode] = cntrArr.length-1;
                 clickCounter("INC");
             }
         //Check the bottom side
-        if (keyCode < cntrArr.length-3 )
-            if (cntrArr[keyCode + 3] == cntrArr.length-1) {
-                cntrArr[keyCode + 3] = cntrArr[keyCode];
+        if (keyCode < cntrArr.length - 4 )
+            if (cntrArr[keyCode + 4] == cntrArr.length -1) {
+                cntrArr[keyCode + 4] = cntrArr[keyCode];
                 cntrArr[keyCode] = cntrArr.length-1;
                 clickCounter("INC");
             }
         //Check the upper side
-        if (keyCode > 2 )
-            if (cntrArr[keyCode - 3] == cntrArr.length-1) {
-                cntrArr[keyCode - 3] = cntrArr[keyCode];
-                cntrArr[keyCode] = cntrArr.length-1;
+        if (keyCode > 3 )
+            if (cntrArr[keyCode - 4] == cntrArr.length-1) {
+                cntrArr[keyCode - 4] = cntrArr[keyCode];
+                cntrArr[keyCode] = cntrArr.length - 1;
                 clickCounter("INC");
             }
         //redisplay the game_board
         displayCntrArr();
     }
     /*
-    Set/reset and read the Count Cliks as needed
-     */
+ Set/reset and read the Count Cliks as needed
+  */
     int clickCounter(String operation){
         Log.d(TAG, "In the clickCounter function");
         switch (operation){
@@ -133,15 +134,12 @@ public class SgameActivity extends AppCompatActivity implements View.OnClickList
         txtClickCount.setText("Click Count : "+ ClickCount);
         //Always return the current count ?
         return ClickCount;
-
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG, "Starting in onCreate");
-        setContentView(R.layout.activity_sgame);
-
+        setContentView(R.layout.activity_fourxfour);
 
         Log.d(TAG, "Starting to setup button array");
         for (int i=0; i < idBtn.length; i++){
@@ -153,10 +151,10 @@ public class SgameActivity extends AppCompatActivity implements View.OnClickList
         Toast.makeText(this, "Welcome to S-Game", Toast.LENGTH_SHORT).show();
         Log.d(TAG, "Initial Button set up");
 
-        btnReset = (Button) findViewById(R.id.btnReset);
-        btnReset.setOnClickListener(this);
-        btnHelp = (Button) findViewById(R.id.btnHelp);
-        btnHelp.setOnClickListener(this);
+        btx4Reset = (Button) findViewById(R.id.btx4Reset);
+        btx4Reset.setOnClickListener(this);
+        btx4Help = (Button) findViewById(R.id.btx4Help);
+        btx4Help.setOnClickListener(this);
         txtClickCount = (TextView) findViewById(R.id.txtClickCount);
 
         setCntrArr();
@@ -164,18 +162,18 @@ public class SgameActivity extends AppCompatActivity implements View.OnClickList
     }
 
     @Override
-    public void onClick(View v){
+    public void onClick(View v) {
         Log.d(TAG, "Inside the On click hander");
-        for (int i =0 ; i<idBtn.length ; i++){
-            if (v.getId() == idBtn[i]){
+        for (int i = 0; i < idBtn.length; i++) {
+            if (v.getId() == idBtn[i]) {
                 userClicks(i);
             }
         }
-        if (isGameOver()){
-            AlertDialog.Builder dialog = new AlertDialog.Builder(SgameActivity.this);
+        if (isGameOver()) {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(fourXfour.this);
             dialog.setCancelable(false);
             dialog.setTitle("SGame! Win");
-            dialog.setMessage("WoW ! You did it in " + ClickCount + ", lets play again... " );
+            dialog.setMessage("WoW ! You did it in " + ClickCount + ", lets play again... ");
             dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int id) {
@@ -188,40 +186,37 @@ public class SgameActivity extends AppCompatActivity implements View.OnClickList
             alert.show();
 
         }
-
         switch(v.getId()){
-             case R.id.btnReset:
-                 Toast.makeText(this, "You choose to Reset S-Game", Toast.LENGTH_SHORT).show();
-                 Log.d(TAG, "Setting up a new S-Game");
+            case R.id.btx4Reset:
+                Toast.makeText(this, "You choose to Reset S-Game", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "Setting up a new S-Game");
 
-                 AlertDialog.Builder dialog = new AlertDialog.Builder(SgameActivity.this);
-                 dialog.setCancelable(false);
-                 dialog.setTitle("SGame! Reset");
-                 dialog.setMessage("Reset ! Are you quitting game... " );
-                 dialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                     @Override
-                     public void onClick(DialogInterface dialog, int id) {
-                         setCntrArr();
-                         displayCntrArr();
-                     }
-                 });
-                 dialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
-                     @Override
-                     public void onClick(DialogInterface dialog, int id) {
-                         Toast.makeText(SgameActivity.this,"Good...you can finish the current game",Toast.LENGTH_SHORT).show();
-                     }
-                 });
-
-                 final AlertDialog alert = dialog.create();
-                 alert.show();
-                 break;
-            case R.id.btnHelp:
+                AlertDialog.Builder dialog = new AlertDialog.Builder(fourXfour.this);
+                dialog.setCancelable(false);
+                dialog.setTitle("SGame! Reset");
+                dialog.setMessage("Reset ! Are you quitting game... " );
+                dialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        setCntrArr();
+                        displayCntrArr();
+                    }
+                });
+                dialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(fourXfour.this,"Good...you can finish the current game",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                final AlertDialog alert = dialog.create();
+                alert.show();
+                break;
+            case R.id.btx4Help:
                 //TODO display the Game instructions
                 Toast.makeText(this, "Please call Subodh for help ;-)", Toast.LENGTH_SHORT).show();
                 Log.d(TAG, "Help Button Clicked");
         }
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
@@ -244,4 +239,5 @@ public class SgameActivity extends AppCompatActivity implements View.OnClickList
         }
         return true;
     }
+
 }
